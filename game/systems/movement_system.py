d7e74a5fed8614297454725b7842bf743bcfd963
Jędrzej Sobków <sobkowjedrzej@gmail.com>
2026-02-06 22:43:26 +0100

    Changed MovementSystem so that crouch while jump does not affect player's standard move speed

--- game/systems/movement_system.py
+++ game/systems/movement_system.py
@@ -3,6 +3,7 @@ from game.core.world import World
 from game.io.input_state import InputState
 from game.components.velocity import Velocity
 from game.components.action import Action
+from game.components.jump_state import JumpState
 from game.core.constants import PLAYER_MOVE_SPEED, PLAYER_CROUCH_SPEED
 
 
@@ -10,15 +11,18 @@ class MovementSystem(System):
         
     def update(self, world: World, dt: float, input_state: InputState = None) -> None:
         
-        entities = world.query(Action, Velocity)
+        entities = world.query(Action, Velocity, JumpState)
         for entity in entities:
             actual_velocity = world.get_component(entity, Velocity)
-        
             action =  world.get_component(entity, Action)
+            j_state = world.get_component(entity, JumpState)
             new_vel_x = 0
             if action.move_left:
                 new_vel_x -= 1
             if action.move_right:
                 new_vel_x += 1
                 
-            actual_velocity.vx = new_vel_x * (PLAYER_CROUCH_SPEED if action.crouch else PLAYER_MOVE_SPEED)
+            if action.crouch and j_state.on_ground:
+                actual_velocity.vx = new_vel_x * PLAYER_CROUCH_SPEED
+            else:
+                actual_velocity.vx = new_vel_x * PLAYER_MOVE_SPEED
